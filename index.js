765d4e5ef2bdc0758d4daa9abe863c1de3f95a5e
VeeStack <150394503+VeeStack@users.noreply.github.com>
2024-04-22 23:05:24 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-'use strict';
-
-// TODO [>=7]: remove me. I've been moved to 'exports' in package.json
-
-/**
-  * A Sequelize module that contains the sequelize entry point.
-  *
-  * @module sequelize
-  */
-
-/** Exports the sequelize entry point. */
-module.exports = require('./lib');
+"use strict";
+module.exports = require("./sequelize");
+//# sourceMappingURL=index.js.map
